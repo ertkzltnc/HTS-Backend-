e51e489422d2513b6a37156336017e039c72f347
ertkzltnc <ertkzltnc@gmail.com>
2020-03-23 19:11:29 +0300

    speciesController

--- api.router.js
+++ api.router.js
@@ -2,10 +2,14 @@ let router=require("express").Router();
 let animalController=require("./controllers/animalController");
 let userController=require("./controllers/userController");
 let herdController=require("./controllers/herdController");
+let speciesController=require("./controllers/speciesController");
+
 
 router.route("/animal").get(animalController.list).post(animalController.create);
 router.route("/animal/:animal_id").put(animalController.update).delete(animalController.delete);
 router.route("/user").post(userController.create).get(userController.login);
 router.route("/herd").get(herdController.list).post(herdController.create);
 router.route("/herd/:herd_id").put(herdController.update).delete(herdController.delete);
+router.route("/species").get(speciesController.list).post(speciesController.create);
+router.route("/species/:species_id").put(speciesController.update).delete(speciesController.delete);
 module.exports=router;
---
+++ controllers/speciesController.js
@@ -0,0 +1,44 @@
+Species = require("../model/species.model");
+response = require("../response");
+
+exports.list = (req, res) => {
+    Species.find({}, (err, species) => {
+        if (err) { return new response(null, err).error500(res) }
+        return new response(species, null).success(res);
+    });
+}
+
+exports.getById = (req, res) => {
+    Species.findById(req.species.species_id, (err, species) => {
+        if (err) { return new response().notFound(res) }
+        return new response(species, null).success(res)
+    })
+}
+
+exports.create = (req, res) => {
+    var species = new Species();
+    species.Name = req.body.Name;   
+    species.save((err) => {
+        if (err) { return new response(null, err).error500(res) }
+        return new response(species, null).created(res);
+    });
+}
+
+exports.update = (req, res) => {
+    Species.findById(req.params.species_id, (err, species) => {
+        if (err) { return response().notFound(res) }
+        species.Name = req.body.Name;        
+        species.save((err) => {
+            if (err) { return new response(null, err).error500(res) }
+            return new response(species, null).success(res);
+        })
+    })
+}
+exports.delete = (req, res) => {
+    Species.findOneAndDelete({ _id:req.params.species_id }, (err, species) => {
+        if (err) { return new response(null, err).error500(res); }
+        if (!species) { return new response().notFound(res); }
+        return new response(species, null).success(res);
+    })
+}
+
